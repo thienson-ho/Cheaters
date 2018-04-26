95875015da0f5a1157ca95a4d56e08745a42d9a3
thienson-ho <thiensonho@utexas.edu>
2018-04-26 11:48:09 -0500

    Attempt to fix

--- src/assignment7/Main.java
+++ src/assignment7/Main.java
@@ -3,35 +3,75 @@ package assignment7;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        File file = new File("src/sm_doc_set");
+        File file = new File(Params.folder);
         ArrayList<String> fileNames = new ArrayList<String>(Arrays.asList(file.list()));
+        System.out.println(fileNames.size());
+        ArrayList<HashMap<String,Boolean>> hashMaps = new ArrayList<>();
 
-        ArrayList<String> arr = new ArrayList<String>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("src/sm_doc_set/" + fileNames.get(2)));
+        for(String fileName: fileNames) {
+            ArrayList<String> arr = new ArrayList<String>();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(Params.folder+"/" + fileName));
 
-            String sCurrentLine;
+                String sCurrentLine;
 
-            while ((sCurrentLine = br.readLine()) != null) {
-                if(!sCurrentLine.equals("")) {
-//                    arr.add(sCurrentLine);
-                    Collections.addAll(arr, sCurrentLine.split(" "));
+                while ((sCurrentLine = br.readLine()) != null) {
+                    if (!sCurrentLine.equals("")) {
+                        //                    arr.add(sCurrentLine);
+                        Collections.addAll(arr, sCurrentLine.split(" "));
+                    }
                 }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            HashMap<String,Boolean> hashMap = new HashMap<>(arr.size() - Params.phraseLength + 1);
+
+            for(int i = 0; i < arr.size() - Params.phraseLength + 1; i++) {
+                StringBuilder phrase = new StringBuilder("");
+                for (int j = i; j < i + Params.phraseLength; j++) {
+                    String word = arr.get(j).toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+                    phrase.append(word);
+                }
+//                System.out.println(phrase.toString());
+                hashMap.put(phrase.toString(),true);
+            }
+
+            hashMaps.add(hashMap);
+
         }
 
-        System.out.println(arr.size());
+        HashMap<String[],Integer> comparisons = new HashMap<>();
+
+        for(int A = 0; A < hashMaps.size()-1; A++) {
+            Set<String> aKeys = hashMaps.get(A).keySet();
+            for(int B = A + 1; B < hashMaps.size(); B++) {
+                Integer hits = 0;
+                HashMap<String,Boolean> bMap = hashMaps.get(B);
+
+                for(String aPhrase: aKeys)  {
+                    if(bMap.get(aPhrase) != null) {
+                        hits++;
+                    }
+                }
+
+                String[] filePair = {fileNames.get(A), fileNames.get(B)};
+                comparisons.put(filePair,hits);
+                if(hits >= Params.filter) {
+                    System.out.println(filePair[0] + " " + filePair[1] + " Hits: " + hits);
+                }
+            }
+        }
+
+
+        System.out.println("Done");
 
 //        System.out.println(arr.get(0));
 //        ArrayList<String> arr1 = new ArrayList<String>();
