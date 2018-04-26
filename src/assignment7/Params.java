a3064a8c85afa036c342054f86952e6aded1235b
thienson-ho <thiensonho@utexas.edu>
2018-04-26 11:51:14 -0500

    adding Params.java

---
+++ src/assignment7/Params.java
@@ -0,0 +1,8 @@
+package assignment7;
+
+public class Params {
+    public static int phraseLength = 4;
+    public static int numThreads = 4;
+    public static int filter = 200;
+    public static String folder = "src/sm_doc_set";
+}
